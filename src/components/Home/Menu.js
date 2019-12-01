23ef7c58e99e8687b3de0357b52397e10ae79cdc
quansean <hongquan1998ltt@gmail.com>
2019-12-01 12:46:21 +0700

    menu

--- src/components/Home/Menu.js
+++ src/components/Home/Menu.js
@@ -1,59 +1,23 @@
 import { Link } from 'react-router-dom'
 
 import React from 'react'
-
+import './style.css'
+// import './nav.js'
 export const Menu = ({ ...props }) => (
 
-  <div
-    className="container-fulid menu"
-    id="myHeader"
-    style={{ height: 'auto', marginBottom:"100px" }}>
-    <nav className="navbar fixed-top navbar-expand-lg navbar-light bg-light ">
-      <div className="container">
-        <p className="navbar-brand" href="#">
-          <img src={window.location.origin+"/images/logoMau.png"} style={{ height: '3vw' }} />
-        </p>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNavAltMarkup"
-          aria-controls="navbarNavAltMarkup"
-          aria-expanded="false"
-          aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon" />
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div className="navbar-nav">
-            <Link
-              to="/home"
-              className="nav-item nav-link active"
-              style={{ cursor: 'pointer' }}>
-              {' '}
-              <p style={{ fontWeight: 'bold', color: '#fff' }}>Trang chủ </p>
-              <span className="sr-only">(current)</span>
-            </Link>
-            <Link
-              to="/user/quest"
-              className="nav-item nav-link active"
-              style={{ cursor: 'pointer' }}>
-              {' '}
-              <p style={{ fontWeight: 'bold', color: '#fff' }}>
-                Thử thách của tôi
-              </p>
-              <span className="sr-only">(current)</span>
-            </Link>
-            <Link
-              to="/user/info"
-              className="nav-item nav-link active"
-              style={{ cursor: 'pointer' }}>
-              {' '}
-              <p style={{ fontWeight: 'bold', color: '#fff' }}>Tài khoản </p>
-              <span className="sr-only">(current)</span>
-            </Link>
-          </div>
-        </div>
-      </div>
-    </nav>
-  </div>
+  <nav className="menu">
+    <div className="logoMenu">
+      <h4 className="logoMenu">BAMBOO QUEST</h4>
+    </div>
+    <ul className="nav-links">
+      <li><Link className="name-item-menu" to="/home">HOME</Link></li>
+      <li><Link className="name-item-menu" to="/user/quest">Cuộc thi của tôi</Link></li>
+      <li><Link className="name-item-menu" to="/user/info">Tài khoản</Link></li>
+    </ul>
+    <div className="burger">
+      <div className="line1"></div>
+      <div className="line2"></div>
+      <div className="line3"></div>
+    </div>
+  </nav>
 )
