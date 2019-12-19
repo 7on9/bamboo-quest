dbd52b8c0faa757eab66ae4c3b1a0c96418c4317
quansean <hongquan1998ltt@gmail.com>
2019-12-19 23:14:00 +0700

    fix home

--- src/components/Home/Item.js
+++ src/components/Home/Item.js
@@ -15,13 +15,13 @@ export const Item = ({ id, title, description, link, img_path }) => {
     <div className="single_courses">
       <div className="thumb" style={{background:'#fff'}}>
         <Link to={link}>
-          <img src={img_path ? img_path : '/images/img_quest_default.png'} alt={title} />
+          <img src={img_path ? img_path : '/images/img_quest_default.png'} style={{height:'250px'}} alt={title} />
         </Link>
       </div>
       <div className="courses_info" style={{background:'#fff'}}>
-        <span>{title}</span>
+        <h4 className='limitText'>{title}</h4>
         <h3>
-          <Link to={link}>{description}</Link>
+          <Link to={link}><p className='limitText'>{description}</p></Link>
         </h3>
         <div className="star_prise d-flex justify-content-between">
           <div className="star">
@@ -29,7 +29,7 @@ export const Item = ({ id, title, description, link, img_path }) => {
             <span>Quan Pham</span>
           </div>
           <div className="prise">
-            <span className="active_prise">{moment(objectIdToDate(id)).format('DD/MM/YYYY')}</span>
+            {/* <span className="active_prise">{moment(objectIdToDate(id)).format('DD/MM/YYYY')}</span> */}
           </div>
         </div>
       </div>
