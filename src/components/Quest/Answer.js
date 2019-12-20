fba8f29238999a7d358d3f00b77a70fa49b55d9e
quansean <hongquan1998ltt@gmail.com>
2019-12-20 21:35:53 +0700

    change size, color

--- src/components/Quest/Answer.js
+++ src/components/Quest/Answer.js
@@ -14,7 +14,7 @@ export const Answer = ({ value, id, label, onAnswer, style }) => {
       <div
         className="center-div cl-ans"
         style={{ ...style, cursor: 'pointer' }}>
-        <h1>{label}</h1>
+        <h1 style={{color:'#fff',fontSize:'4em'}}>{label}</h1>
       </div>
     </Link>
   )
