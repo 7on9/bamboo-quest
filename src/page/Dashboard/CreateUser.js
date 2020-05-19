e4cfef2a9713e20bbd1768e956e9510a38db7aaa
PHQ <hq15ht@gmail.com>
2020-05-19 23:53:00 +0700

    chore: change UI choose a photo

--- src/page/Dashboard/CreateUser.js
+++ src/page/Dashboard/CreateUser.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './comon/styles.css'
 
 export default function CreateUser() {
   return (
@@ -50,21 +51,28 @@ export default function CreateUser() {
                     </div>
                   </div>
                 </div>
-                <div className="form-group">
-                  <label className="col-md-12">Avatar</label>
-                  <div className="col-md-12">
-                    <div className="custom-file">
-                      <input
-                        type="file"
-                        name="avatar_path"
-                        id="customFile"
-                        className="custom-file-input"
-                        //   onChange={handleChange}
-                        // value={values.avatar_path}
-                      />
-                      <label className="custom-file-label" htmlFor="customFile">
-                        Choose file
-                      </label>
+                <div className="row profile-img">
+                  <div className="col-12 choice-img">
+                    <div className="full">
+                      <div className="full">
+                        <img
+                          className="img-upload"
+                          src="/images/img_quest_default.png"
+                        />
+                        <div className="text-center full">
+                          <input
+                            type="file"
+                            id="files"
+                            // onChange={onUploadImage}
+                            hidden={true}
+                          />
+                          <label
+                            for="files"
+                            className="label-input file file-upload-btn ">
+                            Choose a Photo
+                          </label>
+                        </div>
+                      </div>
                     </div>
                   </div>
                 </div>
