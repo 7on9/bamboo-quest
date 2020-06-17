dddece12724cdcccfc4e139e6e608d2660897c7b
long.nguyen <longnt98.dev@gmail.com>
2020-06-17 22:16:51 +0700

    config prod

--- src/common/connection.js
+++ src/common/connection.js
@@ -1,7 +1,7 @@
 //development
-// export const BASE_API_URL = 'http://localhost:2409/api'
-// export const SOCKET_URL = 'http://localhost:2409'
+export const BASE_API_URL = 'http://localhost:2409/api'
+export const SOCKET_URL = 'http://localhost:2409'
 
 //production
-export const BASE_API_URL = 'http://172.104.181.210:2409/api'
-export const SOCKET_URL = 'http://172.104.181.210:2409'
+// export const BASE_API_URL = 'http://172.104.181.210:2409/api'
+// export const SOCKET_URL = 'http://172.104.181.210:2409'
